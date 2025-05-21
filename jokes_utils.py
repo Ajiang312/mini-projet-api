7f0b0bb3d0b61b60e1c4d8fcb3e9bfabc143f173
Ajiang312 <antoine.jiang@esme.fr>
2025-05-21 12:28:24 +0200

    Suppression des fichiers inutiles : utils, vertex_api, jokes_utils

--- jokes_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-from google.cloud import aiplatform
-
-def get_ai_joke():
-    aiplatform.init(project=os.getenv("PROJECT_ID"), location="us-central1")
-    model = aiplatform.TextGenerationModel.from_pretrained("text-bison")
-    response = model.predict(prompt="Raconne une blague marrante")
-    return response.text.strip()
